a2fcaf517df8c6d261eab5672d94e3201c928ce8
Vladislav Neretin <rinosetou@gmail.com>
2022-06-19 16:32:44 +0300

    first

---
+++ html/index.php
@@ -0,0 +1,27 @@
+<?php
+include "../src/function.php";
+
+//task1([1,2,3], false);
+
+//echo task2('/', 15,3,5);
+
+//echo task3(5, 5);
+
+//date_default_timezone_set('Europe/Moscow');
+//echo date('d.m.Y H:i');
+//echo '<br>';
+//echo strtotime('24.02.2016 00:00:00');
+//echo '<br>';
+//echo date('Y-m-d H:i:s', 1456261200);
+//echo '<br>';
+
+//$string = 'Карл у Клары украл Кораллы';
+//echo str_replace('К', '', $string);
+//echo '<br>';
+//
+//$string = 'Две бутылки лимонада';
+//echo str_replace('Две', 'Три', $string);
+
+//echo '<br>';
+//file_put_contents('test.txt', 'Hello again!');
+//my_file_get_contents('test.txt');
